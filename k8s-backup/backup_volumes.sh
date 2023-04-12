42f5d7e2891a0fc2bd72caec8d58eb7b17f64dbb
Chris Debenham <chris@adebenham.com>
2023-04-12 14:40:05 +1000

    reworking sync process

--- k8s-backup/backup_volumes.sh
+++ k8s-backup/backup_volumes.sh
@@ -17,17 +17,22 @@ for VOL in */*
   do if [ ! -z "$1" -a "$1" != "$VOL" ];then continue; fi
   if [ -e ${VOL}/.nodeName ]
     then NODE=`cat ${VOL}/.nodeName`
-    else continue
+    else echo "No node given for ${VOL}"; continue
   fi
   echo Syncing ${VOL} from ${NODE}
-  mkdir -p ${VOLROOT}/${VOL}/
-  rsync -av -e "ssh ${SSH_OPTS}" --delete-after ${PROGRESS} --exclude '.nodeName' --exclude '.pause' root@${NODE}:/k8s/${VOL}/ ${VOLROOT}/${VOL}/
+  if [ "${NODE}" != "jimbob" ]
+    then mkdir -p ${VOLROOT}/${VOL}/
+    rsync -av -e "ssh ${SSH_OPTS}" --delete-after ${PROGRESS} --exclude '.nodeName' --exclude '.pause' root@${NODE}:/k8s/${VOL}/ ${VOLROOT}/${VOL}/
+  else echo "Skipping as source==dest"
+  fi
+  echo -n "${NODE}" > ${VOLROOT}/${VOL}/.nodeName
   if [ "$SKIP_SEND" = "true" ]; then continue; fi
-  for DSTNODE in fanless elite
+  for DSTNODE in fanless elite piserve
     do if [ "${DSTNODE}" != "${NODE}" ]
       then echo Sending ${VOL} to ${DSTNODE}
         rsync -av -e "ssh ${SSH_OPTS}" --delete-after ${PROGRESS} ${VOLROOT}/${VOL}/ root@${DSTNODE}:/k8s/${VOL}/
     fi
   done
+  echo Syncing to tank
   rsync -av -e "ssh ${SSH_OPTS}" --delete-after ${PROGRESS} ${VOLROOT}/${VOL}/ root@jimbob:/tank/Volumes/${VOL}/
 done
--- k8s-restore/restore.sh
+++ k8s-restore/restore.sh
@@ -1,32 +1,40 @@
 #!/bin/sh
+
+echo "192.168.0.2  jimbob" >> /etc/hosts
+echo "192.168.0.4  elite" >> /etc/hosts
+echo "192.168.0.8  fanless" >> /etc/hosts
+echo "192.168.0.9  piserve" >> /etc/hosts
+
 SSH_OPTS='-q -p 12000 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
 echo "Checking for the existing data"
 FORCE=0
 cd /mnt
 for PV in *
-  do if ssh $SSH_OPTS root@jimbob test -f /k8s/${NS}/${PV}-pvc/.pause
+  do echo Checking /k8s/${NS}/${PV}
+  if ssh $SSH_OPTS root@jimbob test -f /k8s/${NS}/${PV}/.pause
     then echo Pausing; sleep 1d
   fi
   # Check for node pv was last on
-  LASTNODE=`ssh $SSH_OPTS root@jimbob cat /k8s/${NS}/${PV}-pvc/.nodeName`
+  LASTNODE=`ssh $SSH_OPTS root@jimbob cat /k8s/${NS}/${PV}/.nodeName`
   if [ -n "$LASTNODE" ]
     then echo Last running on ${LASTNODE} - Now running on ${NODE}
     if [ ! -z "$LASTNODE" -a "$LASTNODE" != "$NODE" ]
       then echo Syncing ${PV} from ${LASTNODE}
       if ping -c 1 ${LASTNODE}
-          then rsync -av -e "ssh $SSH_OPTS" --delete-during root@${LASTNODE}:/k8s/${NS}/${PV}-pvc/ /mnt/${PV}/
+          then rsync -av -e "ssh $SSH_OPTS" --delete-during root@${LASTNODE}:/k8s/${NS}/${PV}/ /mnt/${PV}/
           else FORCE=1
       fi
     fi
   else echo No previous nodeName found
   fi
-  if ssh $SSH_OPTS root@jimbob test -f /k8s/${NS}/${PV}-pvc/.force_restore
+  if ssh $SSH_OPTS root@jimbob test -f /k8s/${NS}/${PV}/.force_restore
   then FORCE=1
   fi
   if [ $FORCE -eq 1 ]
     then echo Restoring ${PV} from backup
-    rsync -av  -e "ssh $SSH_OPTS" --delete-during root@jimbob:/k8s/${NS}/${PV}-pvc/ /mnt/${PV}/
+    rsync -av  -e "ssh $SSH_OPTS" --delete-during root@jimbob:/k8s/${NS}/${PV}/ /mnt/${PV}/
+    FORCE=0
   fi
-  ssh $SSH_OPTS root@jimbob "echo -e $NODE > /k8s/${NS}/${PV}-pvc/.nodeName"
+  ssh $SSH_OPTS root@jimbob "echo -e $NODE > /k8s/${NS}/${PV}/.nodeName"
 done
 echo "  >> Data loaded"
