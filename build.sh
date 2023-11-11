1d911f941ae0db7fb96896cab8ff40c4a5887500
Chris Debenham <chris@adebenham.com>
2023-11-11 19:13:26 +1100

    Enable controlling which archs to build

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+ARCHS=linux/arm/v7,linux/arm64/v8,linux/amd64
+#ARCHS=linux/arm64/v8,linux/amd64
 # shellcheck disable=2013
 docker run --privileged --dns=8.8.8.8 --rm tonistiigi/binfmt --install all
 if [ -n "$1" ]; then
 	pushd "$1" || exit
-	docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag "debenham/${1}:latest" --tag "ghcr.io/cjd/${1}:latest" .
+  docker buildx build --push --platform "$ARCHS" --tag "debenham/${1}:latest" --tag "ghcr.io/cjd/${1}:latest" .
 else
 	for D in $(grep FROM ./*/Dockerfile | cut -f2 -d" " | sort -u); do
 		docker pull "$D"
@@ -11,7 +13,7 @@ else
 	for IMAGE in *; do
 		if [ -d "$IMAGE" ]; then
 			pushd "$IMAGE" || exit
-			docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag "debenham/${IMAGE}:latest" --tag "ghcr.io/cjd/${IMAGE}:latest" .
+			docker buildx build --push --platform "$ARCHS" --tag "debenham/${IMAGE}:latest" --tag "ghcr.io/cjd/${IMAGE}:latest" .
 			popd || exit
 		fi
 	done
