ace351a22d00efc1135d1bbc20481740dc0d51d2
Chris Debenham <chris@adebenham.com>
2024-05-24 18:50:12 +1000

    Add ability to skip some builds/archs

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 #ARCHS=linux/arm/v7,linux/arm64/v8,linux/amd64
 ARCHS=linux/arm64/v8,linux/amd64
+#ARCHS=linux/amd64
 # shellcheck disable=2013
 docker run --privileged --dns=8.8.8.8 --rm tonistiigi/binfmt --install all
 if [ -n "$1" ]; then
 	pushd "$1" || exit
+  if [ -e .build-archs ]; then
+    ARCHS=$(cat .build-archs)
+  fi
   docker buildx build --push --platform "$ARCHS" --tag "debenham/${1}:latest" --tag "ghcr.io/cjd/${1}:latest" .
 else
 	for D in $(grep FROM ./*/Dockerfile | cut -f2 -d" " | sort -u); do
@@ -13,6 +17,9 @@ else
 	for IMAGE in *; do
 		if [ -d "$IMAGE" ]; then
 			pushd "$IMAGE" || exit
+      if [ -e .build-archs ]; then
+        ARCHS=$(cat .build-archs)
+      fi
 			docker buildx build --push --platform "$ARCHS" --tag "debenham/${IMAGE}:latest" --tag "ghcr.io/cjd/${IMAGE}:latest" .
 			popd || exit
 		fi
