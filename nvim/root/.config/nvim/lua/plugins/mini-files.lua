f1ed69234f49a6c81ee1e43b5734f868e4e0b25e
Chris Debenham <chris@adebenham.com>
2024-06-19 08:22:04 +1000

    nvim plugins updates

--- nvim/root/.config/nvim/lua/plugins/mini-files.lua
+++ nvim/root/.config/nvim/lua/plugins/mini-files.lua
@@ -3,6 +3,22 @@ return {
   version = false,
   keys = { { "<leader>e", ":lua MiniFiles.open()<cr>", { slient=true, desc = "Files"}}},
   config = function()
-    require('mini.files').setup()
+    require('mini.files').setup(
+    {
+      mappings = {
+        close       = 'q',
+        go_in       = '<S-Right>',
+        go_in_plus  = '<Enter>',
+        go_out      = '<S-Left>',
+        go_out_plus = 'H',
+        reset       = '<BS>',
+        reveal_cwd  = '@',
+        show_help   = 'g?',
+        synchronize = '=',
+        trim_left   = '<',
+        trim_right  = '>',
+      }
+    }
+    )
   end
 }
