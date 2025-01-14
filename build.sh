c824e8726bce281029ef91ce875c2abcc12988a8
Chris Debenham <chris@adebenham.com>
2025-01-14 19:02:40 +1100

    Don't build armv7

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-#ARCHS=linux/arm/v7,linux/arm64/v8,linux/amd64
 ARCHS=linux/arm64/v8,linux/amd64
-#ARCHS=linux/amd64
-# shellcheck disable=2013
 docker run --privileged --dns=8.8.8.8 --rm tonistiigi/binfmt --install all
 if [ -n "$1" ]; then
 	pushd "$1" || exit
@@ -11,6 +8,7 @@ if [ -n "$1" ]; then
   fi
   docker buildx build --push --platform "$ARCHS" --tag "debenham/${1}:latest" --tag "ghcr.io/cjd/${1}:latest" .
 else
+	# shellcheck disable=2013
 	for D in $(grep FROM ./*/Dockerfile | cut -f2 -d" " | sort -u); do
 		docker pull "$D"
 	done
