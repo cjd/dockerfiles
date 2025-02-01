4ade0ea5ee56e203735cd2a8302787965120adfe
Chris Debenham <chris@adebenham.com>
2025-02-01 20:07:58 +1100

    Simplify backup/restore scripts

--- k8s-backup/backup_volumes.sh
+++ k8s-backup/backup_volumes.sh
@@ -1,74 +1,32 @@
 #!/bin/bash
 SSH_OPTS='-q -p 12000 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
-PROGRESS=""
-SKIP_SEND=""
-if [ "$1" = "-v" ]
-  then PROGRESS="--progress"
-  shift
-fi
-if [ "$1" = "-b" ]
-  then SKIP_SEND="true"
-  shift
-fi
 
 VOLROOT=/k8s
 cd ${VOLROOT} || exit
 
-function handle_sigterm()
-{
+function handle_sigterm() {
   echo SIGTERM received - shutting down
-  for VOL in */*
-    do echo "Syncing $VOL to tank"
-    echo -n "tank" > "${VOLROOT}/${VOL}/.nodeName"
-    rsync -av -e "ssh ${SSH_OPTS}" --delete-during ${PROGRESS} "${VOLROOT}/${VOL}/" "root@jimbob:/tank/Volumes/${VOL}/"
+  for VOL in */*; do
+    echo "Syncing $VOL to tank"
+    echo -n "tank" >"${VOLROOT}/${VOL}/.nodeName"
+    rsync -avx -e "ssh ${SSH_OPTS}" --delete-during --exclude "lost+found" "${VOLROOT}/${VOL}/" "root@nas.default:/tank/Volumes/${VOL}/"
   done
   echo Shutdown Complete
   exit
 }
 
-if [ "$1" = "-c" ]; then
-  cp /root/.ssh/id_rsa /
-  chmod 0400 /id_rsa
-  SSH_OPTS="${SSH_OPTS} -i /id_rsa"
-  trap handle_sigterm SIGTERM
-  while true
-    do echo Waiting 2h
-    sleep 2h &
-    wait $!
-    for VOL in */*
-      do echo "Syncing $VOL to tank"
-      echo -n "tank" > "${VOLROOT}/${VOL}/.nodeName"
-      ionice -c 3 rsync -av -e "ssh ${SSH_OPTS}" --delete-during ${PROGRESS} "${VOLROOT}/${VOL}/" "root@jimbob:/tank/Volumes/${VOL}/"
-    done
-    echo Sync Complete
+cp /root/.ssh/id_rsa /
+chmod 0400 /id_rsa
+SSH_OPTS="${SSH_OPTS} -i /id_rsa"
+trap handle_sigterm SIGTERM
+while true; do
+  echo Waiting 2h
+  sleep 2h &
+  wait $!
+  for VOL in */*; do
+    echo "Syncing $VOL to tank"
+    echo -n "tank" >"${VOLROOT}/${VOL}/.nodeName"
+    ionice -c 3 rsync -avx -e "ssh ${SSH_OPTS}" --delete-during --exclude "lost+found" "${VOLROOT}/${VOL}/" "root@nas.default:/tank/Volumes/${VOL}/"
   done
-  exit
-fi
-
-for VOL in default/* monitoring/*
-  do if [ -n "$1" ] && [ "$1" != "$VOL" ];then continue; fi
-  if [ -e "${VOL}/.nodeName" ]; then
-    NODE=$(cat "${VOL}/.nodeName")
-  else echo "No node given for ${VOL}"
-    continue
-  fi
-  echo Syncing "${VOL}" from "${NODE}"
-  if [ "${NODE}" = "tank" ]; then
-    echo Skipping as set to tank
-    continue
-  elif [ "${NODE}" = "jimbob" ]; then
-    echo "Skipping as source==dest"
-  else mkdir -p "${VOLROOT}/${VOL}/"
-    ionice -c 3 rsync -av -e "ssh ${SSH_OPTS}" --delete-during ${PROGRESS} --exclude '.nodeName' --exclude '.pause' "root@${NODE}:/k8s/${VOL}/" "${VOLROOT}/${VOL}/"
-  fi
-  echo -n "${NODE}" > "${VOLROOT}/${VOL}/.nodeName"
-  if [ "$SKIP_SEND" = "true" ]; then continue; fi
-  for DSTNODE in elite piserve lenny
-    do if [ "${DSTNODE}" != "${NODE}" ]
-      then echo "Sending ${VOL} to ${DSTNODE}"
-        ionice -c 3 rsync -av -e "ssh ${SSH_OPTS}" --delete-during ${PROGRESS} "${VOLROOT}/${VOL}/" "root@${DSTNODE}:/k8s/${VOL}/"
-    fi
-  done
-  echo Syncing to tank
-  ionice -c 3 rsync -av -e "ssh ${SSH_OPTS}" --delete-during ${PROGRESS} "${VOLROOT}/${VOL}/" "root@jimbob:/tank/Volumes/${VOL}/"
+  echo Sync Complete
 done
--- k8s-restore/restore.sh
+++ k8s-restore/restore.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
-# shellcheck disable=SC2086
-{
-echo "192.168.0.2  jimbob"
-echo "192.168.0.4  elite"
-echo "192.168.0.5  lenny"
-echo "192.168.0.8  fanless"
-echo "192.168.0.9  piserve"
-} >> /etc/hosts
-
 # Fix for permissions when pod not running as root (https://github.com/kubernetes/kubernetes/issues/57923)
 cp /root/.ssh/id_rsa /root
 chmod 0400 /root/id_rsa
@@ -15,51 +6,15 @@ chmod 0400 /root/id_rsa
 SSH_OPTS='-q -p 12000 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /root/id_rsa'
 
 echo "Checking for the existing data"
-FORCE=0
-if [ -z "${NS}" ];
-then echo "NS not set"
-  cd /k8s || exit
-  for PV in */*
-  do if [ -d "$PV" ]; then
-      echo Checking ${PV}
-      if [ ! -e "${PV}/.nodeName" ]; then
-        # Check for node pv was last on
-        echo Restoring ${PV} from tank
-        rsync -av -e "ssh $SSH_OPTS" --delete-during root@jimbob:/tank/Volumes/${PV}/ /k8s/${PV}/
-      fi
+cd /k8s || exit
+for PV in */*; do
+  if [ -d "$PV" ]; then
+    echo "Checking ${PV}"
+    # Check if directory empty
+    if [ ! "$(ls -A "${PV}" | grep -v lost+found)" ]; then
+      echo "Restoring ${PV} from tank"
+      rsync -av -e "ssh $SSH_OPTS" --delete-during root@nas.default:/tank/Volumes/${PV}/ /k8s/${PV}/
     fi
-  done
-  exit
-fi
-
-cd /mnt || exit
-for PV in *
-  do echo "Checking /k8s/${NS}/${PV}"
-  if ssh $SSH_OPTS root@jimbob test -f /k8s/${NS}/${PV}/.pause
-    then echo Pausing; sleep 1d
   fi
-  # Check for node pv was last on
-  LASTNODE=$(ssh $SSH_OPTS root@jimbob cat /k8s/${NS}/${PV}/.nodeName)
-  if [ -n "$LASTNODE" ]
-    then echo Last running on ${LASTNODE} - Now running on ${NODE}
-    if [ -n "$LASTNODE" ] && [ "$LASTNODE" != "$NODE" ]
-      then echo Syncing ${PV} from ${LASTNODE}
-      if ping -c 1 ${LASTNODE}
-          then rsync -av -e "ssh $SSH_OPTS" --delete-during root@${LASTNODE}:/k8s/${NS}/${PV}/ /mnt/${PV}/
-          else FORCE=1
-      fi
-    fi
-  else echo No previous nodeName found
-  fi
-  if ssh $SSH_OPTS root@jimbob test -f /k8s/${NS}/${PV}/.force_restore
-    then FORCE=1
-  fi
-  if [ $FORCE -eq 1 ]
-    then echo Restoring ${PV} from backup
-    rsync -av  -e "ssh $SSH_OPTS" --delete-during root@jimbob:/k8s/${NS}/${PV}/ /mnt/${PV}/
-    FORCE=0
-  fi
-  # shellcheck disable=SC2029
-  ssh $SSH_OPTS root@jimbob "echo -e ${NODE} > /k8s/${NS}/${PV}/.nodeName"
 done
 echo "  >> Data loaded"
